288d3ab00737cc5babf3a83cd63a4a899c6bd403
DevanshSK <95367443+DevanshSK@users.noreply.github.com>
2022-02-08 23:14:38 +0530

    Initialising project 1

---
+++ main.js
@@ -0,0 +1,8 @@
+//Initalise project
+const canvas = document.querySelector('canvas');
+
+//Width and height of canvas.
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+console.log(canvas);
